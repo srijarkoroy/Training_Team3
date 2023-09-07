039e66d1f63c16927215f751f3befb6ba9fbe575
a19tripathi <a19tripathi@gmail.com>
2023-09-07 12:46:11 +0530

    initial commit

---
+++ backend/src/main/java/com/example/training/TrainingGroup3Application.java
@@ -0,0 +1,13 @@
+package com.example.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingGroup3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingGroup3Application.class, args);
+	}
+
+}
