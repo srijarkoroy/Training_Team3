1ac29cebca2aa1cc4d262b1aa92cb46666788b76
Ritik Naik <ritiknaik23@gmail.com>
2023-09-08 11:42:24 +0530

    Added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login'
+import Navbar from './components/Navbar'
 import SignUp from './components/SignUp'
 import {Link, Route, Routes} from 'react-router-dom'
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<div><Login /> <Navbar /> </div>} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
 
