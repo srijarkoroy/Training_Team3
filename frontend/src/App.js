505b83893b7025c8ce0faf3abe0646025a95e3f7
Arjun J <arjunjay2001@gmail.com>
2023-09-08 14:26:35 +0530

    reorganise frontend and add signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login'
 import SignUp from './components/SignUp'
-import {Link, Route, Routes} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
+    <div >
+      <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
