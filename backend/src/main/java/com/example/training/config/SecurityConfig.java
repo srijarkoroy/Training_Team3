c2f5cb8ea3f21dea1f1355b8cda76dd6ba28fc0b
a19tripathi <88593745+a19tripathi@users.noreply.github.com>
2023-09-13 00:42:10 +0530

    refactor

--- backend/src/main/java/com/example/training/config/SecurityConfig.java
+++ backend/src/main/java/com/example/training/config/SecurityConfig.java
@@ -27,17 +27,7 @@ public class SecurityConfig {
     private JwtAuthFilter authFilter;
 
     @Bean
-    //authentication
     public UserDetailsService userDetailsService() {
-//        UserDetails admin = User.withUsername("Basant")
-//                .password(encoder.encode("Pwd1"))
-//                .roles("ADMIN")
-//                .build();
-//        UserDetails user = User.withUsername("John")
-//                .password(encoder.encode("Pwd2"))
-//                .roles("USER","ADMIN","HR")
-//                .build();
-//        return new InMemoryUserDetailsManager(admin, user);
         return new UserInfoUserDetailsService();
     }
 
