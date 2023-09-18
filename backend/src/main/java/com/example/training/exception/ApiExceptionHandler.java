c211e2353b7afc8a94f17d5a5d7ca2fad8581e2a
a19tripathi <a19tripathi@gmail.com>
2023-09-19 01:00:23 +0530

    refactor

---
+++ backend/src/main/java/com/example/training/exception/ApiExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.training.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ErrorResponse errorResponse = new ErrorResponse("500", "Null Fields Present");
+        return new ResponseEntity<>(errorResponse, headers, status);
+    }
+}
---
+++ backend/src/main/java/com/example/training/exception/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.example.training.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class ErrorResponse {
+
+    private String code;
+    private String message;
+}
