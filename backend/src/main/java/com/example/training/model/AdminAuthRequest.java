d2ee270a8ee04e32e9083ae816e7262bbc5b980f
srijarkoroy <sr8962@srmist.edu.in>
2023-09-20 08:27:35 +0530

    feat: admin login authorization (init)

---
+++ backend/src/main/java/com/example/training/model/AdminAuthRequest.java
@@ -0,0 +1,14 @@
+package com.example.training.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminAuthRequest {
+
+    private String id;
+    private String password;
+}
