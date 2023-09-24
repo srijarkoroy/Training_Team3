0fa07593ac31a9f88a51a4a72028302760539f10
a19tripathi <88593745+a19tripathi@users.noreply.github.com>
2023-09-24 22:52:42 +0530

    add jwt exception handling

--- backend/src/main/java/com/example/training/filter/JwtAuthFilter.java
+++ backend/src/main/java/com/example/training/filter/JwtAuthFilter.java
@@ -2,6 +2,7 @@ package com.example.training.filter;
 
 import com.example.training.service.JwtService;
 import com.example.training.util.UserInfoUserDetailsService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,6 +16,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
@@ -27,7 +33,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authHeader = request.getHeader("Authorization");
+        try{String authHeader = request.getHeader("Authorization");
         String token = null;
         String username = null;
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
@@ -35,14 +41,28 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             username = jwtService.extractUsername(token);
         }
 
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-            if (jwtService.validateToken(token, userDetails)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                if (jwtService.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
             }
-        }
+        }catch (Exception exception){
+//            log.error("Error logging in : {} ", exception.getMessage());
+            response.setHeader("error",exception.getMessage());
+            response.setStatus(UNAUTHORIZED.value());
+            Map<String,Object > error = new HashMap<>();
+//            error.put("error_message",exception.getMessage());
+            error.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+            error.put("error", "Unauthorized");
+            error.put("message", exception.getMessage());
+            error.put("path", request.getServletPath());
+            response.setContentType(APPLICATION_JSON_VALUE);
+            new ObjectMapper().writeValue(response.getOutputStream(),error);
+    }
         filterChain.doFilter(request, response);
     }
 }
